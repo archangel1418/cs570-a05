43b1615f26365668570efa459018cbeb6708e356
Mark <mark619park@gmail.com>
2020-04-13 15:38:27 -0700

    Update A05.cpp

--- A05.cpp
+++ A05.cpp
@@ -6,9 +6,9 @@
 //mutex variable to control the buffer access
 sem_t mutex;
 //will keep track of the number of items in the buffer
-sem_t ItemsInBuffer;
+sem_t ItemsOnBelt;
 //will track the number of spaces avaialable in the buffer
-sem_t OpenSpotsInBuffer;
+sem_t OpenSpaceOnBelt;
 
 char buf[BUFFERSIZE];
 
@@ -17,10 +17,10 @@ int main(int argc, char *argv[]){
 
     // initialize mutex, its a binary mutex so it is either going to be 0 or 1
     sem_init(&mutex, 0, 1);
-    //init the size of ItemsInBuffer, starts at 0 because nothing has been addem
-    sem_init(&ItemsInBuffer, 0, 0);
+    //init the size of ItemsOnBelt, starts at 0 because nothing has been addem
+    sem_init(&ItemsOnBelt, 0, 0);
     //init size of Open spots, starts at buffer size cuz all space is available
-    sem_init(&OpenSpotsInBuffer, 0, BUFFERSIZE);
+    sem_init(&OpenSpaceOnBelt, 0, BUFFERSIZE);
 
 
 }
@@ -31,14 +31,14 @@ void *produce(int *index){
     int currProduceIndex = *startIndex;
     while(true){
         // protect from overflow and control buffer
-        sem_wait(&OpenSpotsInBuffer);
+        sem_wait(&OpenSpaceOnBelt);
         sem_wait(&mutex);
         //add item to buffer
         buf[currProduceIndex] = currProduceIndex;
 
         //notifiy the end of this process
         sem_post(&mutex);
-        sem_post(&ItemsInBuffer);
+        sem_post(&ItemsOnBelt);
         currProduceIndex = (currProduceIndex+1) % BUFFERSIZE;
     }
 }
@@ -47,11 +47,11 @@ void *consume(int *index){
         int * startIndex = index;
         int currentConsumeIndex = *startIndex;
         while(true){
-            sem_wait(&ItemsInBuffer);
+            sem_wait(&ItemsOnBelt);
             sem_wait(&mutex);
             //remove the candy here from the buffer
             sem_post(&mutex);
-            sem_post(&OpenSpotsInBuffer);
+            sem_post(&OpenSpaceOnBelt);
             currentConsumeIndex = (currentConsumeIndex +1) % BUFFERSIZE;
 
         }
