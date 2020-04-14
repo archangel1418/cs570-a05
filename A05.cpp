e85266ada8f32137cf1d77caefcb0c5c42a0f1e5
Mark <mark619park@gmail.com>
2020-04-13 18:30:50 -0700

    Update

--- A05.cpp
+++ A05.cpp
@@ -10,8 +10,13 @@ sem_t OpenSpaceOnBelt;
 Candy belt[BELTSIZE];
 int frogcounter;
 
+
 int main(int argc, char *argv[])
 {
+    for (int i=0 ; i<BELTSIZE; i++){
+        belt[i].name = "";
+    }
+
     pthread_t thread1, thread2;
 
     // initialize mutex, its a binary mutex so it is either going to be 0 or 1
@@ -26,6 +31,9 @@ void *produce()
 {
     //intitalize the start //this should be where we produce the item
     Candy nextCandy = createCandy();
+    if (frogcounter>=3){
+        nextCandy.name = "escargot suckers";
+    }
     //define starting index which is 0
     int index = 0;
     while (true)
@@ -36,6 +44,9 @@ void *produce()
         sem_wait(&mutex1);
         //add item to buffer
         belt[index] = nextCandy;
+        if (nextCandy.name=="froggy bites"){
+        frogcounter++;
+        }
 
         //notifiy the end of this process
         sem_post(&mutex1);
@@ -53,8 +64,16 @@ void *consume(int *index)
         sem_wait(&ItemsOnBelt);
         sem_wait(&mutex1);
         //remove the candy here from the buffer
+        if(belt[currentConsumeIndex].name== "froggy bites"){
+            frogcounter--;
+        }
+        belt[currentConsumeIndex].name = "";
         sem_post(&mutex1);
         sem_post(&OpenSpaceOnBelt);
         currentConsumeIndex = (currentConsumeIndex + 1) % BELTSIZE;
     }
 }
+
+//get index for producer by checking null in the belt array, return first index that is null
+
+//get index for consumer by checking for not null in the belt array, return first index that is not null
